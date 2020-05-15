6eec025dd53928de6df80584905be85172f504e6
IvanKalinic <ivankalinic1998@gmail.com>
2020-05-15 18:30:48 +0200

    Adding navbar with buttons and routes

--- dioklecijanovalegija-vue/src/App.vue
+++ dioklecijanovalegija-vue/src/App.vue
@@ -1,60 +1,70 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer v-model="sideNav" temporary absolute>
+      <v-list class="onebyone">
+        <v-list-tile 
+        v-for="item in menuItems" 
+        :key="item.title"
+        >
+          <v-btn text
+          router 
+        :to="item.link">
+            <v-list-tile-action>
+              <v-icon left>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>{{item.title}}</v-list-tile-content>
+          </v-btn>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-navbar>
+      <v-toolbar dark class="indigo darken-4">
+        <span class="hidden-sm-and-up">
+          <v-app-bar-nav-icon @click.native.stop="sideNav= !sideNav"></v-app-bar-nav-icon>
+        </span>
+        <v-toolbar-title><router-link to="/" tag="span" style="cursor:pointer">Dev Meetup</router-link></v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn 
+          text 
+          v-for="item in menuItems" 
+          :key="item.title"
+          router 
+          :to="item.link">
+            <!--  flat class="indigo accent-3"  -->
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-app-navbar>
+    <main>
+      <router-view></router-view>
+    </main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-}
+  data() {
+    return {
+      sideNav: false,
+      menuItems: [
+        { icon: "supervisor_account", title: "View Meetups" ,link:'/meetups'},
+        { icon: "room", title: "Organize Meetups",link:'/meetup/new' },
+        { icon: "person", title: "Profile",link:'/profile' },
+        { icon: "face", title: "Sign up",link:'/signup' },
+        { icon: "lock_open", title: "Sign in",link:'/signin' }
+      ]
+    };
+  }
+};
 </script>
+
+<style scoped>
+.onebyone {
+  display: flex;
+  flex-flow: column wrap;
+}
+</style>
